f5474b92a9ad6189cc5f2c9d8f578e9466968a27
Eric Murphy <ericnmurphy@gmail.com>
2018-08-22 14:37:39 +0700

    Basic form functionality

--- src/components/login/MobileLogin.js
+++ src/components/login/MobileLogin.js
@@ -1,9 +1,57 @@
 import React, { Component } from "react";
-import Firebase from "firebase/app";
+import { Form, Text } from "informed";
+import { isEmail, isEmpty } from "validator";
+import firebase from "firebase/app";
 import "firebase/auth";
+import config from "./firebaseConfig";
+
+const auth = firebase.initializeApp(config);
 
 export default class MobileLogin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.setFormApi = this.setFormApi.bind(this);
+  }
+
+  handleSubmit() {
+    const { email, password } = this.formApi.getState().values;
+    console.log(email, password);
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  setFormApi(formApi) {
+    this.formApi = formApi;
+  }
+
   render() {
-    return <div>Test</div>;
+    const validateEmail = value => {
+      return !isEmail(String(value)) ? "Please enter a valid email." : null;
+    };
+
+    const validatePassword = value => {
+      return isEmpty(String(value)) ? "Please enter a password." : null;
+    };
+
+    return (
+      <Form onSubmit={this.handleSubmit} getApi={this.setFormApi}>
+        <label htmlFor="email">Email:</label>
+        <Text field="email" id="email" validate={validateEmail} />
+        <label htmlFor="password">Password:</label>
+        <Text
+          type="password"
+          field="password"
+          id="password"
+          validate={validatePassword}
+        />
+        <button type="submit">Log in</button>
+      </Form>
+    );
   }
 }
